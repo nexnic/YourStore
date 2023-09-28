03cfbb2fd074a9a6ad48faeed3e1f7ba56cf3d04
Kent Erik <kent.erik.hole@outlook.com>
2023-09-28 12:59:03 +0200

    start on default nav

---
+++ src/script/pages/Navigation.tsx
@@ -0,0 +1,15 @@
+export default function Navigation() {
+    return(
+        <div className="navBar">
+                <div>
+                    <h1>YourName</h1>
+                </div>
+                <div>
+                    <input type="text" placeholder="Search..." />
+                </div>
+                <div>
+                    <i className="fa-solid fa-cart-shopping"></i>
+                </div>
+        </div>
+    )
+}
