e7db2fe6ec17d97c7000aca600db218ec66cd9c8
Kent Erik <kent.erik.hole@outlook.com>
2023-09-28 15:20:44 +0200

    fix bug on main

--- src/script/pages/Layout.tsx
+++ src/script/pages/Layout.tsx
@@ -1,13 +1,17 @@
 import Header from './Header'
 import Footer from './Footer'
+import { Outlet} from "react-router-dom";
 
 
 export default function Layout({pageConfig}){
 
+    
     return (
        <>
         <Header pageConfig={pageConfig}/>
+        <Outlet/>
         <Footer pageConfig={pageConfig}/>
+        
         </>
     )
 }
