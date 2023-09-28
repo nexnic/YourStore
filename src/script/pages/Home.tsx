54f9349c12b7580acfc71007ca2f280b2bdc45df
Kent Erik <kent.erik.hole@outlook.com>
2023-09-28 15:27:46 +0200

    adding API to data

--- src/script/pages/Home.tsx
+++ src/script/pages/Home.tsx
@@ -1,12 +1,46 @@
-
+import React, { useState, useEffect } from 'react';
 
 
 
 
 export default function HomePage() {
-  return (
-    <main>
-      <h2>Main</h2>
-    </main>
-  )
+  document.title = `yourPAge | Home`
+  
+  const { data, isLoading, isError } = RequestAPI(
+    'https://jsonplaceholder.typicode.com/todos',
+  );
+
+
+    if(isLoading) return <div>loading..</div>
+    if(isError) return <div>error</div>
+    return (
+      <main>
+        {data}
+      </main>
+    )
+  
+}
+
+function RequestAPI(url) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+    useEffect(()=> {
+      async function fetchAPI() {
+        try {
+          setIsLoading(true);
+          setIsError(false);
+          const fetchedData = await fetch(url);
+          const json = await fetchedData.json();
+          setData(json);
+        } catch (error) {
+          console.log(error);
+          setIsError(true);
+        }
+        setIsLoading(false);
+      }
+      fetchAPI()
+    }, [url])
+    return { data, isLoading, isError };
 }
