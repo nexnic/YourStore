81d51664192e0d7378f312906b4d14972215bac1
Kent Erik <kent.erik.hole@outlook.com>
2023-09-28 13:14:06 +0200

    adding layout and nav to Router

--- src/script/App.tsx
+++ src/script/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Import Page Config
 import HomePage from './pages/Home';
 import PageNotFound from './pages/PageNotFound';
+import Layout from './pages/Layout';
+
 
 export default function App() {
     return (
     <BrowserRouter>
             <Routes>
-                <Route index element={<HomePage/>}></Route>
+                <Route path='/' element={<Layout></Layout>}/>
+                <Route index element={<HomePage />} />
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
     </BrowserRouter>
