60c9b874a5f756b3194d4d892cbc1b2fb7ddd4fa
Kent Erik <kent.erik.hole@outlook.com>
2023-09-28 13:12:05 +0200

    Adding nav templet

--- src/script/pages/Navigation.tsx
+++ src/script/pages/Navigation.tsx
@@ -1,14 +1,29 @@
+// Import 
+    // Reach
+        import { Link } from "react-router-dom";
+import Input from '../Components/Input'
+
+
 export default function Navigation() {
     return(
         <div className="navBar">
-                <div>
-                    <h1>YourName</h1>
+                <div className="navBar__top">
+                    <div>
+                        <h1>YourName</h1>
+                    </div>
+                    <div>
+                        <Input Class={'navbar__search'} Type={'text'} PlaceHolder={'Search...'}/>
+                    </div>
+                    <div>
+                        <i className="fa-solid fa-cart-shopping"></i>
+                    </div>
                 </div>
-                <div>
-                    <input type="text" placeholder="Search..." />
-                </div>
-                <div>
-                    <i className="fa-solid fa-cart-shopping"></i>
+                <div className="navBar__bottom">
+                    <ul>
+                        <li>
+                            <Link to="/" className="link__navbar">Home</Link>
+                        </li>
+                    </ul>
                 </div>
         </div>
     )
