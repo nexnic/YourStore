8eabe55223340ea6639cc61ed163ad18c8f2ed8e
Kent Erik <kent.erik.hole@outlook.com>
2023-09-29 15:56:39 +0200

    passing function to navigation

--- src/script/pages/Header.tsx
+++ src/script/pages/Header.tsx
@@ -1,10 +1,10 @@
 import Navigation from "./Navigation"
 
-export default function Header ({pageConfig, cart}) {
+export default function Header ({pageConfig, cart, onSelectEmptyCart}) {
     console.log(cart)
     return (
         <header className="header">
-            <Navigation pageConfig={pageConfig} cart={cart}/>
+            <Navigation pageConfig={pageConfig} cart={cart} onSelectEmptyCart={onSelectEmptyCart}/>
         </header>
     )
 }
