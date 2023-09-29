e27278a69f6e4b33730279b9f11664962b01ba85
Kent Erik <kent.erik.hole@outlook.com>
2023-09-29 12:20:57 +0200

    adding

--- src/script/pages/Home.tsx
+++ src/script/pages/Home.tsx
@@ -1,46 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import Card from './Card';
 
-
-
-
-export default function HomePage() {
+export default function HomePage({storeItem}) {
   document.title = `yourPAge | Home`
   
-  const { data, isLoading, isError } = RequestAPI(
-    'https://jsonplaceholder.typicode.com/todos',
-  );
-
-
-    if(isLoading) return <div>loading..</div>
-    if(isError) return <div>error</div>
-    return (
-      <main>
-        {data}
-      </main>
-    )
+  return (
+    <main>
+        <Card storeItem={storeItem}/>
+    </main>
+  )
   
 }
 
-function RequestAPI(url) {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-    useEffect(()=> {
-      async function fetchAPI() {
-        try {
-          setIsLoading(true);
-          setIsError(false);
-          const fetchedData = await fetch(url);
-          const json = await fetchedData.json();
-          setData(json);
-        } catch (error) {
-          console.log(error);
-          setIsError(true);
-        }
-        setIsLoading(false);
-      }
-      fetchAPI()
-    }, [url])
-    return { data, isLoading, isError };
-}
