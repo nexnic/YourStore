9e3e2083989d0bd34ae9fa69b7e1fd76199ffe40
Kent Erik <kent.erik.hole@outlook.com>
2023-10-03 18:20:00 +0200

    Working on

---
+++ src/script/pages/ProductPage/Components/ProductReviews.tsx
@@ -0,0 +1,9 @@
+export default function ProductReviews({test}) {
+    console.log(test)
+    return (
+       <li>
+        <p>"Jim N."</p>
+        <small>My partner loves it, its her favourite.</small>
+       </li>
+    )
+}
