edbcc653c9d636df9ee73ecff1331c07126b6c6c
Kent Erik <kent.erik.hole@outlook.com>
2023-10-03 14:04:43 +0200

    rebuild

--- src/script/Components/ProductList.tsx
+++ src/script/Components/ProductList.tsx
@@ -1,11 +1,11 @@
 import ProductCard from './productcard'
 
-export default function ProductList({storeItem, onSelectAddCart}) {
+export default function ProductList({items}) {
     
     return (
         <section className='section__product'>
-            {storeItem.map((item) => (
-                <ProductCard item={item} key={item.id} onSelectAddCart={onSelectAddCart}/>
+            {items.map((item) => (
+                <ProductCard item={item} key={item.id}/>
             ))}
         </section>
     )
--- src/script/Components/productcard.tsx
+++ src/script/Components/productcard.tsx
@@ -1,18 +1,26 @@
-import Button from "./Button"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function ProductCard({item, onSelectAddCart}) {
-    const {id, title, imageUrl, price} = item 
+
+export default function ProductCard({item}) {
+    const {id, title, imageUrl} = item 
+
+    const navigate = useNavigate()
+
+    function handlereClick(id) {
+        navigate(`/product/${id}`)
+    }
 
     return (
-        <div className="card" id={id}>
+        <div className="card" id={id} onClick={()=> handlereClick(item.id)}>
             <img src={imageUrl} alt={title}/>
             <div className="card__text">
                 <h4>{title}</h4>
             </div>
             <div className="card__btn">
-                <Button onClick={()=>onSelectAddCart(item)}>{price}</Button>
             </div>
         </div>
     )
 
 }
+
