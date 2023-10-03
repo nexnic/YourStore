1db34124d9f0070bcdc010bad80b3a4620a0bb24
Kent Erik <kent.erik.hole@outlook.com>
2023-10-03 14:02:44 +0200

    adding working file

--- src/script/pages/Home/Home.tsx
+++ src/script/pages/Home/Home.tsx
@@ -1,12 +1,45 @@
+import { useEffect, useState } from "react";
+import LoadingScreen from '../../Components/LoadingScreen'
 import ProductList from '../../Components/ProductList'
 
-export default function HomePage({storeItem, onSelectAddCart}) {
-  document.title = `yourPAge | Home`
-  
+export default function HomePage() {
+  const [items , setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    async function getItem() {
+      try {
+        const response = await fetch('https://api.noroff.dev/api/v1/online-shop')
+        const json = await response.json()
+
+        if(response.status === 200){
+          setItems(...[json])
+          setIsLoading(false)
+        }
+        if(response.status === 300 >= 400){
+          console.log('fail')
+        }
+
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getItem() 
+  }, [])
+
+
+  if(isLoading) {
+    return (
+      <main>
+        <LoadingScreen></LoadingScreen>
+      </main>
+    )
+  }
   return (
-    <main>
-        <ProductList storeItem={storeItem} onSelectAddCart={onSelectAddCart}/>
-    </main>
+    <div>
+      <ProductList items={items} />
+    </div>
   )
   
 }
