f298f31d2e4637b9b0d6dd9c97e7d0d0c508f5cf
Kent Erik <kent.erik.hole@outlook.com>
2023-10-03 18:19:06 +0200

    adding spec

---
+++ src/script/pages/ProductPage/Components/ProductSpec.tsx
@@ -0,0 +1,21 @@
+
+
+export default function ProductSpec({items}) {
+
+    
+    return(
+        <div>
+            <div>
+                <div className="">
+                    <h3>Product info</h3>
+                </div>
+                <div>
+                    {items.description}
+                </div>
+            </div>
+            
+            
+
+        </div>
+    )
+}
