ff01e0f4684b4f593d76a9416033e04695996704
Kent Erik <kent.erik.hole@outlook.com>
2023-10-03 18:37:28 +0200

    done with Aboutpage

--- src/script/pages/AboutUs/AboutUsPage.tsx
+++ src/script/pages/AboutUs/AboutUsPage.tsx
@@ -1,7 +1,11 @@
+import Story from "./Components/Story"
+import Team from '../AboutUs/Components/Team'
+
 export default function AboutusPage() {
     return (
         <div>
-            About us
+            <Story/>
+            <Team/>
         </div>
     )
 }
