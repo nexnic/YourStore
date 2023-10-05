981ecb16eb1e823fa7ff3e47b12558df58bdd701
Kent Erik <kent.erik.hole@outlook.com>
2023-10-05 13:00:03 +0200

    adding function and made a new check function

--- src/script/Components/ProductList.tsx
+++ src/script/Components/ProductList.tsx
@@ -1,11 +1,11 @@
 import ProductCard from './productcard'
 
-export default function ProductList({items}) {
+export default function ProductList({items, onSelectAddCart}) {
     
     return (
         <section className='section__product'>
             {items.map((item) => (
-                <ProductCard item={item} key={item.id}/>
+                <ProductCard item={item} key={item.id} onSelectAddCart={onSelectAddCart}/>
             ))}
         </section>
     )
