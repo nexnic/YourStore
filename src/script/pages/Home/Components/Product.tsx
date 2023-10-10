617b6f025c8faae253e8abf8e018b4a1d1a5bbdf
Kent Erik <kent.erik.hole@outlook.com>
2023-10-10 13:01:15 +0200

    fix on product and styling

--- src/script/pages/Home/Components/Product.tsx
+++ src/script/pages/Home/Components/Product.tsx
@@ -1,16 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
+
 export default function Product ({item, onSelectAddCart}) {
     const {id, price, title, imageUrl, description} = item
+    const navigate = useNavigate();
 
+    function handlereClick(event, id) {
+       
+        if(event.target.localName === 'button')  onSelectAddCart(id)
+        else navigate(`/product/${id}`);
+    }
     
     return (
-        <div className="card" id={id}>
-            <img src={imageUrl} alt={title} />
-            <div>
-                <h3>{title}</h3>
+        <div className="card" id={id} onClick={()=> handlereClick(event, id)}>
+            <img src={imageUrl} alt={title} className="card__img"/>
+            <div className="card__details">
+                <h3 className="headline__card">{title}</h3>
                 <p>{description}</p>
                 <small>{price}</small>
             </div>
-            <button onClick={() => onSelectAddCart(id)}>{price}</button>
+            <button onClick={() => handlereClick(event, id)}>{price}</button>
         </div>
     )
 }
