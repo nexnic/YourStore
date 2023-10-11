d4813b50a7bdf6c471af3cd62f9c1b6d537e7192
Kent Erik <kent.erik.hole@outlook.com>
2023-10-11 17:11:21 +0200

    fiks styling and adding prosent

--- src/script/pages/Home/Components/Product.tsx
+++ src/script/pages/Home/Components/Product.tsx
@@ -2,8 +2,12 @@ import { useNavigate } from 'react-router-dom';
 
 
 export default function Product ({item, onSelectAddCart}) {
-    const {id, price, title, imageUrl, description} = item
+    const {id, price, title, imageUrl, description, discountedPrice} = item
+    const isOnSale = price > discountedPrice
+    const discountedProsent = ((1 - discountedPrice / price) * 100).toFixed(0)
     const navigate = useNavigate();
+
+
 
     function handlereClick(event, id, item) {
        
@@ -14,12 +18,13 @@ export default function Product ({item, onSelectAddCart}) {
     return (
         <div className="card" id={id} onClick={()=> handlereClick(event, id)}>
             <img src={imageUrl} alt={title} className="card__img"/>
+            {isOnSale ? <div className='card__sale'>{discountedProsent} %</div> : null}
             <div className="card__details">
                 <h3 className="headline__card">{title}</h3>
                 <p>{description}</p>
-                <small>{price}</small>
+                {isOnSale ?  <small><span>{discountedPrice}</span> <span>{price}</span></small>: <small>{price}</small>}
             </div>
-            <button onClick={() => handlereClick(event, id, item)}>{price}</button>
+            <button onClick={() => handlereClick(event, id, item)} className='btn__buy'>{price}</button>
         </div>
     )
 }
