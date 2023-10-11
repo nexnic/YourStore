eed8206f0f47cc17e99bf0c9583fa3a53881fe53
Kent Erik <kent.erik.hole@outlook.com>
2023-10-11 10:10:18 +0200

    working on product page Laout

--- src/script/pages/ProductPage/Components/ProductHeading.tsx
+++ src/script/pages/ProductPage/Components/ProductHeading.tsx
@@ -1,14 +1,10 @@
 export default function ProductHeading ({items}) {
 
     return (
-        <div>
-            <div>
+        <div className="productPage__heading">
                 <h2>{items.title}</h2>
                 <p>{items.description}</p>
                 <small>Item number: {items.id}</small>
-            </div>
-           
-            <img src={items.imageUrl} alt={items.title}/>
         </div>
     )
 }
