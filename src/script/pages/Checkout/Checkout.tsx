9cade529c4d2edec9766ad0508697343481484de
Kent Erik <kent.erik.hole@outlook.com>
2023-10-14 18:08:42 +0200

    start on the layout

--- src/script/pages/Checkout/Checkout.tsx
+++ src/script/pages/Checkout/Checkout.tsx
@@ -1,14 +1,10 @@
 
-import CheckoutDetailts from './Components/CheckoutDetails'
-import CheckoutList from './Components/CheckoutList'
 
 export default function CheckoutPage({cart}) {
-
+    console.log(cart)
     return (
         <main>
             <section className='Checkout__productList'>
-            <CheckoutDetailts/>
-            <CheckoutList cart={cart}/> 
             </section>
         </main>
     )
