53d8c857289b4f553d80554bf0b916ff5d1bd0d1
Kent Erik <kent.erik.hole@outlook.com>
2023-10-15 15:16:18 +0200

    adding rewiese

--- src/script/pages/ProductPage/Components/ProductSpec.tsx
+++ src/script/pages/ProductPage/Components/ProductSpec.tsx
@@ -1,15 +1,18 @@
+import ProductReviews from "./ProductReviews"
 
 // @ts-ignore
 export default function ProductSpec({items}) {
-
+    const {description, reviews} = items
     
     return(
         <div className="productPage__spec">
                 <div>
                     <h3>Product info</h3>
+                    {description}
                 </div>
                 <div>
-                    {items.description}
+                    <h3>Reviews</h3>
+                    {reviews ? <ul>{reviews.map((review) => <ProductReviews review={review} key={review.id}/> )} </ul> : <p> No Review</p>}
                 </div>
         </div>
     )
